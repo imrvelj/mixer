fd3369f8c637122c9b8ac76f84d571358d3cc99b
Ivan Mrvelj <imrvelj@protonmail.com>
2020-10-31 17:29:12 +0100

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  name: 'App',
+  setup() {
+    return {
+      name: 'Mixer'
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="app">
+    <h2>{{name}}</h2>
+  </div>
+</template>
